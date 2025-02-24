67b6c02a99e69f2a493215e8a08ec7526680f132
mooncodesss <sunnymahala63@gmail.com>
2025-02-24 17:49:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NKMODNK7/AviaxMusic",
+    "https://github.com/mooncodesss/moom-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
