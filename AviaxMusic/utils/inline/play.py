867bbe28c10eb350da81eb0c0aa84c857fcd9e51
mooncodesss <sunnymahala63@gmail.com>
2025-02-24 18:07:23 +0530

    Update play.py

--- AviaxMusic/utils/inline/play.py
+++ AviaxMusic/utils/inline/play.py
@@ -66,7 +66,6 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -80,7 +79,6 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
