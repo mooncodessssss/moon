308953021ca939512a01a1e219bdb73370d02d69
mooncodesss <sunnymahala63@gmail.com>
2025-02-25 08:47:18 +0530

    Update broadcast.py

--- AviaxMusic/plugins/misc/broadcast.py
+++ AviaxMusic/plugins/misc/broadcast.py
@@ -184,7 +184,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_5"])
         text = _["broad_6"]
-        from RessoMusic.core.userbot import assistants
+        from AviaxMusic.core.userbot import assistants
 
         for num in assistants:
             sent = 0
